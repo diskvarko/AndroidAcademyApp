ea5215851e9d6738a6f667b07b7b2ac6a5391e60
Diana Skvarko <diaskvarko@gmail.com>
2020-11-28 10:45:54 +0300

    temp commit

--- app/src/main/java/com/diskvarko/androidacademyapp/MainActivity.kt
+++ app/src/main/java/com/diskvarko/androidacademyapp/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.diskvarko.androidacademyapp
 
-import android.content.Intent
-import android.os.Bundle
+import  android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.appcompat.app.AppCompatActivity
+import com.diskvarko.androidacademyapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
-
+    // private lateinit var binding: ActivityMainBinding
+     private lateinit var someFragment: FragmentMoviesList
+    // val firstFrag = FragmentMoviesList()
+    // val secondFrag = FragmentMoviesDetails()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, FragmentDetailsActivity::class.java)
-        startActivity(intent)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, FragmentMoviesList())
+                .commit()
+
+
     }
 
 
-
+//     fun showFilmDetails() {
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.main_container, FragmentMoviesDetails())
+//            commit()
+//        }
+//
+//    }
 
 }
+interface FragmentClick {
+    fun showFilmDetails()
+    fun toFirstFragment()
+}
+
+
+
+
